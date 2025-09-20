a0c8e0b8cd9d8bd2511b7f264a7754fcaab90358
Sadman Al Sakib <150785896+saadmanshakiib@users.noreply.github.com>
2025-09-21 02:59:23 +0600

    Update ProfileRepository.java

--- src/main/java/com/example/Repository/ProfileRepository.java
+++ src/main/java/com/example/Repository/ProfileRepository.java
@@ -3,6 +3,9 @@ package com.example.Repository;
 import com.example.Entity.Profile;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ProfileRepository extends JpaRepository<Profile,Long> {
+    Optional<Profile> findByUserName(String useRName);
 
 }
